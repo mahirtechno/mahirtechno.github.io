ab549688653df9cf205bab2ef9767a9509de5fe8
AbidKhairyAK <abid.khairy.ak@gmail.com>
2020-07-29 21:51:23 +0700

    init

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [
+    './public/**/*.html'
+  ],
+  theme: {
+    extend: {
+    	borderRadius: {
+    		'large': '7.5rem'
+    	}
+    },
+  },
+  variants: {},
+  plugins: [],
+}
